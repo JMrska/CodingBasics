6578c354f14c9b03c5a2230bb12d2681c84f4b73
jmrska <j.mrska@gmail.com>
2015-04-21 06:08:23 +0200

    bkbkjb
    Conflicts:
    	CodeTesting/src/base/Main.java

--- CodeTesting/src/base/Main.java
+++ CodeTesting/src/base/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("TADA");
-System.out.println("cg");
-System.out.println("");
-System.out.println("");
-System.out.println("made in master");
+		System.out.println("TADA");
+		System.out.println("cg");
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("made in master");
+		
+		System.out.println("in dev");
+		System.out.println("second in dev");
+
 	}
 
 }
