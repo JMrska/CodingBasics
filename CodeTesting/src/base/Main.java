d571216a640cce880b6661cc523bee8d1fdfa664
jmrska <j.mrska@gmail.com>
2015-04-22 22:04:11 +0200

    local

--- CodeTesting/src/base/Main.java
+++ CodeTesting/src/base/Main.java
@@ -17,6 +17,7 @@ public class Main {
 		System.out.println("second in server");
 		System.out.println("third in server");
 		System.out.println("third in local");
+		System.out.println("third in local");
 
 	}
 
