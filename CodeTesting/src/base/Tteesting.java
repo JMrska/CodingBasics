a72dbd4ca292cccc68778646b4e5e684b1a910f0
jmrska <j.mrska@gmail.com>
2015-04-22 22:17:04 +0200

    vjvjhjhv

--- CodeTesting/src/base/Tteesting.java
+++ CodeTesting/src/base/Tteesting.java
@@ -1,5 +1,12 @@
 package base;
 
 public class Tteesting {
+<<<<<<< Upstream, based on origin/master
   System.out.println("server") ;
+=======
+
+	public static void main(String[] args) {
+		System.out.println("local");
+	}
+>>>>>>> e9acd0e vjvjhjhv
 }
