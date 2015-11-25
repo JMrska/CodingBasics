1085b1a4c634f183040d35e7fb312b514192038b
jmrska <j.mrska@gmail.com>
2015-11-25 21:09:31 +0100

    fast devel

--- CodeTesting/src/base/Tteesting.java
+++ CodeTesting/src/base/Tteesting.java
@@ -2,7 +2,7 @@ package base;
 
 public class Tteesting {
 	public static void main(String[] args) {
-	System.out.println("server") ;
+	System.out.println("fast devel") ;
   System.out.println("local");
 	}
 }
