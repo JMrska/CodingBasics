fae1e0e947f9423fd1299d72c80c791a707ff1c1
jmrska <j.mrska@gmail.com>
2015-11-25 21:14:09 +0100

    crazy2

--- CodeTesting/src/base/Tteesting.java
+++ CodeTesting/src/base/Tteesting.java
@@ -3,6 +3,7 @@ package base;
 public class Tteesting {
 	public static void main(String[] args) {
 	System.out.println("crazy devel") ;	
+	System.out.println("crazy devel2") ;	
   System.out.println("local");
 	}
 }
